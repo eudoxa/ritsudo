f89d9b190749ab9aaa8b3e28f31c6b8682744444
shingo morita <morita.shingo@gmail.com>
2019-04-27 21:30:21 +0900

    show option

--- lib/ritsudo/cli.rb
+++ lib/ritsudo/cli.rb
@@ -3,30 +3,32 @@ require 'thor'
 module Ritsudo
   class Cli < Thor
     default_command :benchmark
-    option :url, required: true, aliases: ['-a']
-    option :count, default: 5, type: :numeric, aliases: ['-c']
-    option :cookies, type: :string, aliases: ['-C']
-    option :sub_process_timeout, default: 5, type: :numeric, aliases: ['-s']
-    option :timeout, default: 10, type: :numeric, aliases: ['-t']
-    option :wait_time, default: 1, type: :numeric, aliases: ['-w']
-    option :ua, type: :string, aliases: ['-u']
-    option :match, type: :string, aliases: ['-m']
+    class_option :url, required: true, aliases: ['-a']
+    class_option :count, default: 5, type: :numeric, aliases: ['-c']
+    class_option :cookies, type: :string, aliases: ['-C']
+    class_option :sub_process_timeout, default: 5, type: :numeric, aliases: ['-s']
+    class_option :timeout, default: 10, type: :numeric, aliases: ['-t']
+    class_option :wait_time, default: 1, type: :numeric, aliases: ['-w']
+    class_option :ua, type: :string, aliases: ['-u']
+    class_option :match, type: :string, aliases: ['-m']
+
     desc "benchmark URL", "benchmark"
     def benchmark()
-      match = options[:match] ? Regexp.new(options[:match]) : nil
+      match = class_options[:match] ? Regexp.new(class_options[:match]) : nil
       collector =  Ritsudo::Collector.new(match: match)
       benchmark = Ritsudo::Benchmark.new(collector: collector)
-      benchmark.do(options[:url],
-                   count: options[:count],
-                   sub_process_timeout: options[:sub_process_timeout],
+      benchmark.do(class_options[:url],
+                   count: class_options[:count],
+                   sub_process_timeout: class_options[:sub_process_timeout],
                    driver_options: {
-                     timeout: options[:timeout],
-                     wait_time: options[:wait_time],
-                     user_agent: options[:ua],
-                     cookies: options[:cookies]
+                     timeout: class_options[:timeout],
+                     wait_time: class_options[:wait_time],
+                     user_agent: class_options[:ua],
+                     cookies: class_options[:cookies]
                    }
                   )
       benchmark.collector.report
     end
+    tasks["benchmark"].options = self.class_options
   end
 end
