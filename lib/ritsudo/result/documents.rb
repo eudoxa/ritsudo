45ba696fcf2db7a9971bf89eb6e7efa56f6e9950
shingo morita <morita.shingo@gmail.com>
2019-04-27 20:49:05 +0900

    Document use uncompletable module as well as others.

--- lib/ritsudo/result/documents.rb
+++ lib/ritsudo/result/documents.rb
@@ -1,23 +1,9 @@
 module Ritsudo
   module Result
     class Documents < Base
-      def add(request)
-        @completed[request.url] << request.time
-      end
-
-      def report
-        table_data = []
-        puts "[Document]"
-        @completed.each do |url, complete|
-          table_data << {
-            url: url[0..100],
-            avg: (complete.sum(0.0) / complete.length).round(2),
-            max: complete.max.round(2),
-            min: complete.min.round(2),
-            count: complete.size
-          }
-        end
-        Formatador.display_table(table_data, [:url, :avg, :max, :min, :count])
+      include Ritsudo::Result::Uncompletable
+      def name
+        "Document"
       end
     end
   end
